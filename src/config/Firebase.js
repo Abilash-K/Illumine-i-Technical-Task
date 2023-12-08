7d65379975a212aabf7ff4f2bcd61113d3c4f3a4
Abi <abilash.konganar@gmail.com>
2023-12-08 17:39:10 +0530

    feat: Initialize React project with Firebase configuration
    Added Firebase SDK and configured project settings in the React app.
    - Installed Firebase dependencies
    - Set up Firebase configuration with API keys and project settings

---
+++ src/config/Firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHwuoQGV3lT_vAuA3we3EIz8P-XfFFvno",
+  authDomain: "technical-assessment-4d71b.firebaseapp.com",
+  projectId: "technical-assessment-4d71b",
+  storageBucket: "technical-assessment-4d71b.appspot.com",
+  messagingSenderId: "215936095564",
+  appId: "1:215936095564:web:41b9f77ba59978ff89ebe9",
+  measurementId: "G-BB6CGB0F9J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
